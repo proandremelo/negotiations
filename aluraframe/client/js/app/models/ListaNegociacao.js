61b0ec21716d2cb44238a1b9f79643dbc947ee39
André Melo <proandremelo@gmail.com>
2021-05-31 18:00:49 -0300

    Uploading course jsA1-Alura

---
+++ aluraframe/client/js/app/models/ListaNegociacao.js
@@ -0,0 +1,16 @@
+class ListaNegociacao{
+
+    constructor(){
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes(){
+        return [].concat(this._negociacoes);
+    }
+
+
+}
